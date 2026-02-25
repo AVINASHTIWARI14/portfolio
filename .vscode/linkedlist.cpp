acc40aff51d46eb5f2fdfdf90024eb19241394c1
Avinash Tiwari <avinashtiwari2005ws@gmail.com>
2026-02-25 10:01:28 +0530

    Initial commit

---
+++ .vscode/linkedlist.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+class listnode{
+    public:
+    int val;
+    listnode*next;
+    listnode(int x){
+        val=x;
+        next=null ptr;
+    }
+};
+listnode*insertathead(listnode*head,int value ){
+       listnode*newnode=new listnode(value);
+       newnode->next=
+    }
+}
+int main(){
+    
+    return 0;
+}
